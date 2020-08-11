f6205c0a6aa482dac50ac30545fbc434a708306f
Shivani Pharande <45893136+shivanirp@users.noreply.github.com>
2020-08-11 18:02:32 +0530

    Update Lab3.java

--- Lab3.java
+++ Lab3.java
@@ -26,13 +26,14 @@ class Emp
 }
 class EmpManager
 {
-	int count = 2;
+	int count = 5;
 	private Emp[] arr=new Emp[count];
 	Scanner s=new Scanner(System.in);
-	public void create(Emp e)
+	public void create()
 	{
 		for(int i=0;i<count;i++)
 		{
+			
 			arr[i]=new Emp();
 			System.out.print("Enter id:");
 			arr[i].setEmpno(s.nextInt());
@@ -48,10 +49,47 @@ class EmpManager
 		{
 			System.out.println("ID : "+arr[i].getEmpno()+", Name : "+arr[i].getName()+", Salary : "+arr[i].getSalary());
 		}
+		System.out.println("\n********************************************\n");
+
 	}
-	public void update(int item, Emp e)
+	public void update(int item)
 	{
+		int flag=0;
+		for(int i=0;i<count;i++)
+		{
+			if(arr[i].getEmpno()==item)
+			{
+				System.out.println("Enter updated salary: ");
+				arr[i].setSalary(s.nextDouble());
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0)
+		{
+			System.out.println("Item not found");
+		}
+	}
+	public void delete(int item)
+	{
+		ArrayList<Emp> list=new ArrayList<Emp>(Arrays.asList(arr));
 		
+		int flag=0;
+		for(int i=0;i<count;i++)
+		{
+			if(list.get(i).getEmpno()==item)
+			{
+				list.remove(i);
+				flag=1;
+				count--;
+				list.toArray(arr);
+				break;
+			}
+		}
+		if(flag==0)
+		{
+			System.out.println("Item not found");
+		}
 	}
 	
 }
@@ -62,8 +100,68 @@ public class Demo1 {
 		Scanner s=new Scanner(System.in);
 		EmpManager em=new EmpManager();
 		Emp e=new Emp();
-		em.create(e);
+		em.create();
+		em.print();
+		System.out.println("Enter empno to be updated:");
+		int n=s.nextInt();
+		em.update(n);
+		em.print();
+		System.out.println("Enter empno to be deleted:");
+		int d=s.nextInt();
+		System.out.println("After deleting empno :"+d);
+		em.delete(d);
 		em.print();
 	}
 	
 }
+
+
+/*
+ * 
+ * OUTPUT
+ * Enter id:1
+Enter name:aa
+Enter salary:111
+Enter id:2
+sEnter name:s
+Enter salary:222
+Enter id:3
+Enter name:dd
+Enter salary:333
+Enter id:4
+Enter name:ff
+Enter salary:444
+Enter id:5
+Enter name:gg
+Enter salary:555
+ID : 1, Name : aa, Salary : 111.0
+ID : 2, Name : ss, Salary : 222.0
+ID : 3, Name : dd, Salary : 333.0
+ID : 4, Name : ff, Salary : 444.0
+ID : 5, Name : gg, Salary : 555.0
+
+********************************************
+
+Enter empno to be updated:
+2
+Enter updated salary: 
+888
+ID : 1, Name : aa, Salary : 111.0
+ID : 2, Name : ss, Salary : 888.0
+ID : 3, Name : dd, Salary : 333.0
+ID : 4, Name : ff, Salary : 444.0
+ID : 5, Name : gg, Salary : 555.0
+
+********************************************
+
+Enter empno to be deleted:
+4
+After deleting empno :4
+ID : 1, Name : aa, Salary : 111.0
+ID : 2, Name : ss, Salary : 888.0
+ID : 3, Name : dd, Salary : 333.0
+ID : 5, Name : gg, Salary : 555.0
+
+********************************************
+ * */
+
