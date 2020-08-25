21df978b441c5e96e21ff3d0d8b6a1bfffcad935
Shivani Pharande <45893136+shivanirp@users.noreply.github.com>
2020-08-25 19:56:00 +0530

    Create Shape.java

---
+++ java/Day2_ShapeAssignment/Shape.java
@@ -0,0 +1,30 @@
+package ShapeAssignment;
+
+public class Shape {
+	
+	private String name;
+
+	public Shape() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Shape(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Name=" + name;
+	}
+
+}
