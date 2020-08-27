ddc1830f79294a98d58a69640b01b1ba59df3bfb
Shivani Pharande <45893136+shivanirp@users.noreply.github.com>
2020-08-27 14:00:41 +0530

    Create PersonNotFoundException.java

---
+++ java/Day3_PersonAssignment/PersonNotFoundException.java
@@ -0,0 +1,31 @@
+package com.exception;
+
+public class PersonNotFoundException extends Exception {
+
+	private String message;
+
+	public PersonNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public PersonNotFoundException(String message) {
+		super();
+		this.message=message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		
+		this.message = message;
+	}
+	
+	
+	public void displayMessage() {
+		
+		System.out.println(getMessage());
+		
+	}
+}
